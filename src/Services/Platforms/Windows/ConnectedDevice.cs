890efa4a9fdcc7dd4f34c209b32114a0d6272d9f
Jeff Johnson <turbo25mike@gmail.com>
2024-04-09 14:17:29 -0700

    fixing restore

--- src/Services/Platforms/Windows/ConnectedDevice.cs
+++ src/Services/Platforms/Windows/ConnectedDevice.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Security.Cryptography;
@@ -33,46 +35,26 @@ public class ConnectedDevice : IConnectedDevice
     private void Gatt_MaxPduSizeChanged(object? sender, object e) =>
         MTU = (_Gatt is null || _Device.ConnectionStatus == BluetoothConnectionStatus.Disconnected) ? 20 : (uint)_Gatt.MaxPduSize;
 
-    public async void Write(string serviceID, string characteristicID, byte[] val, bool withResponse = true)
+    public Task<IBLERequest> Write(IBLERequest request)
     {
-        var ch = GetCharacteristic(serviceID, characteristicID);
+        var ch = GetCharacteristic(request.ServiceID, request.CharacteristicID);
 
         if (ch is null) return;
         _WriteInProgress = true;
-        var writeType = withResponse ? GattWriteOption.WriteWithResponse : GattWriteOption.WriteWithoutResponse;
+        var writeType = request.WithResponse ? GattWriteOption.WriteWithResponse : GattWriteOption.WriteWithoutResponse;
         await ch.WriteValueAsync(CryptographicBuffer.CreateFromByteArray(val), writeType);
+        return request;
     }
 
-    public async void Read(string serviceID, string characteristicID, Action<KeyValuePair<string, byte[]?>> action, bool notify)
-    {
-        try
-        {
-            _ReadAction = action;
-            var deviceId = BLEUtils.ParseDeviceId(_Device.BluetoothAddress).ToString();
-            var ch = GetCharacteristic(serviceID, characteristicID);
-
-            if (ch is null) return;
-
-            if (notify)
-            {
-                ch.ValueChanged += ValueChanged;
-                var result = await ch.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
-            }
-            else
-            {
-                var readResult = await ch.ReadValueAsync(BluetoothCacheMode.Uncached);
-                _ReadAction.Invoke(new KeyValuePair<string, byte[]?>(_Device.DeviceId, readResult.Value?.ToArray()));
-            }
-        } catch (Exception ex)
-        {
-
-        }
-    }
-
-    private void ValueChanged(object sender, GattValueChangedEventArgs e)
+    public Task<IBLERequest> Read(IBLERequest request)
     {
         var deviceId = BLEUtils.ParseDeviceId(_Device.BluetoothAddress).ToString();
-        _ReadAction?.Invoke(new KeyValuePair<string, byte[]?>(deviceId, e.CharacteristicValue?.ToArray()));
+        var ch = GetCharacteristic(serviceID, characteristicID);
+
+        if (ch is null) return;
+
+        var readResult = await ch.ReadValueAsync(BluetoothCacheMode.Uncached);
+        request.SetResponse(readResult.Value?.ToArray());
     }
 
     public void Dispose()
@@ -83,6 +65,23 @@ public class ConnectedDevice : IConnectedDevice
             service.Session?.Dispose();
             service.Dispose();
         }
+    }
+
+    public async Task StartNotifying(string serviceID, string characteristicID)
+    {
+        var ch = GetCharacteristic(serviceID, characteristicID);
+
+        if (ch is null) return;
+
+        ch.ValueChanged -= ValueChanged;
+        ch.ValueChanged += ValueChanged;
+        var result = await ch.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
+    }
+
+    private void ValueChanged(object sender, GattValueChangedEventArgs e)
+    {
+        var deviceId = BLEUtils.ParseDeviceId(_Device.BluetoothAddress).ToString();
+        _ReadAction?.Invoke(new KeyValuePair<string, byte[]?>(deviceId, e.CharacteristicValue?.ToArray()));
     }
 
     public async void StopNotifying(string serviceID, string characteristicID)
@@ -98,12 +97,12 @@ public class ConnectedDevice : IConnectedDevice
         return _Services.Select(s => new BLEService(s.Key.Uuid.ToString().ToUpper(), s.Value.Select(c => new BLECharacteristic(c.Uuid.ToString().ToUpper(), c.UserDescription))));
     }
 
-    private KeyValuePair<GattDeviceService, IEnumerable<GattCharacteristic>>? GetService(string uuid) 
+    private KeyValuePair<GattDeviceService, IEnumerable<GattCharacteristic>>? GetService(string uuid)
     {
         if (_Services is null) return null;
         return _Services.FirstOrDefault(s => s.Key.Uuid.ToString().Equals(uuid, StringComparison.CurrentCultureIgnoreCase));
     }
-    
+
     private GattCharacteristic GetCharacteristic(string serviceID, string uuid)
     {
         var service = GetService(serviceID);
@@ -111,7 +110,7 @@ public class ConnectedDevice : IConnectedDevice
         var ch = service?.Value.FirstOrDefault(s => s.Uuid.ToString().Equals(uuid, StringComparison.CurrentCultureIgnoreCase));
         return ch;
     }
-        
+
     private Dictionary<GattDeviceService, IEnumerable<GattCharacteristic>> _Services;
 
     public bool CanSendWriteWithoutResponse() => !_WriteInProgress;
